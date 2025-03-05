8fbbb4c001bfa3944f46f158ff6264299569ea52
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-05 21:53:43 +0300

    start project

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
