d04beb57485cfb1ff12050ca7272dcc373016762
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-11 23:39:00 +0300

    add new class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,13 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name? ");
+        String username = scanner.next();
+        System.out.println("Hello, " + username + "!");
+        scanner.close();
+    }
 }
