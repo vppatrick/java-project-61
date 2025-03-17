235f7085d5b4d815a3a9d5841bd2245088d63356
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-18 01:28:46 +0300

    update App classe

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,28 @@
 package hexlet.code;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        System.out.println("Please enter the game number and press Enter."
+                + "\n1 - Greet"
+                + "\n2 - Even"
+                + "\n0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        String numOfGame = scanner.next();
+        String username;
+        switch (numOfGame) {
+            case "1":
+                username = Cli.greeting(scanner);
+                break;
+            case "2":
+                Even.startGame(scanner);
+                break;
+            default:
+                System.exit(0);
+                break;
+        }
+        scanner.close();
     }
 }
