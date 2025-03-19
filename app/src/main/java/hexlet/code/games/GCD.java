dcbe708893c45b42e1cbc8d01505dd2e051cc472
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-19 23:50:20 +0300

    update GCD class

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -3,21 +3,22 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 import java.util.Random;
 
-public class Even {
+public class GCD {
     public static void startGame() {
-        String instruction = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        String instruction = "Find the greatest common divisor of given numbers.";
         Random random = new Random();
         String[] questions = new String[3];
         String[] answers = new String[3];
+
         for (int i = 0; i < 3; i++) {
-            int number = random.nextInt(100);
-            questions[i] = Integer.toString(number);
-            if (number % 2 == 0) {
-                answers[i] = "yes";
-            } else {
-                answers[i] = "no";
-            }
+            int number1 = random.nextInt(100);
+            int number2 = random.nextInt(100);
+            questions[i] = number1 + " " + number2;
+            answers[i] = Integer.toString(calcGCD(number1, number2));
         }
         Engine.start(instruction, questions, answers);
+    }
+    private static int calcGCD(int a, int b) {
+        return (a % b == 0) ? Math.abs(b) : calcGCD(b, a % b);
     }
 }
