fa56a1901d05e5500559225462cf69a9f620a0d6
vppatrick <57995026+vppatrick@users.noreply.github.com>
2025-03-20 00:14:56 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,26 +2,23 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("java")
-    id("se.patrikerdes.use-latest-versions") version "0.2.18"
-    id("com.github.ben-manes.versions") version "0.41.0"
     application
     jacoco
-    checkstyle
+    id("java")
+    id("checkstyle")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
     // Плагин для публикации отчета о покрытии тестами на SonarQube
     id("org.sonarqube") version "6.0.1.5171"
 }
 
-application {
-    mainClass = "hexlet.code.App"
-}
-
 group = "hexlet.code"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+application { mainClass = "hexlet.code.App" }
+
+repositories { mavenCentral() }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
