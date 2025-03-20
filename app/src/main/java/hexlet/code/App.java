3dcc889a320f7fb0919adc97242e0c4862e88155
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-20 20:40:53 +0300

    update App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
