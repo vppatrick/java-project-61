c259caf9166d2d5e10a6eef4fa7bcd2050f13671
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-23 19:28:36 +0300

    update Calc class

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Calc {
     public static final int COUNT_OF_STEP_GAME = 3;
     public static final int RANDOM_UPPER_LIMIT = 10;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     public static void startGame() {
         String instruction = "What is the result of the expression?";
         String[] questions = new String[COUNT_OF_STEP_GAME];
@@ -14,9 +14,9 @@ public class Calc {
         Character[] ops = {'+', '-', '*'};
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
-            int number1 = random.nextInt(RANDOM_UPPER_LIMIT);
-            int number2 = random.nextInt(RANDOM_UPPER_LIMIT);
-            int index = random.nextInt(COUNT_OF_STEP_GAME);
+            int number1 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
+            int number2 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
+            int index = RANDOM.nextInt(COUNT_OF_STEP_GAME);
             questions[i] = number1 + " " + ops[index] + " " + number2;
 
             int result = 0;
