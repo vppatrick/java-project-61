c255a73ddbe9a4a89bb1d95ceea0677e376e1f2f
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-23 19:03:24 +0300

    update Calc class

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,19 +4,19 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Calc {
+    public static final int COUNT_OF_STEP_GAME = 3;
+    public static final int RANDOM_UPPER_LIMIT = 10;
     public static void startGame() {
         String instruction = "What is the result of the expression?";
         Random random = new Random();
-        int count = 3;
-        String[] questions = new String[count];
-        String[] answers = new String[count];
+        String[] questions = new String[COUNT_OF_STEP_GAME];
+        String[] answers = new String[COUNT_OF_STEP_GAME];
         Character[] ops = {'+', '-', '*'};
 
-        int limit = 10;
-        for (int i = 0; i < count; i++) {
-            int number1 = random.nextInt(limit);
-            int number2 = random.nextInt(limit);
-            int index = random.nextInt(count);
+        for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
+            int number1 = random.nextInt(RANDOM_UPPER_LIMIT);
+            int number2 = random.nextInt(RANDOM_UPPER_LIMIT);
+            int index = random.nextInt(COUNT_OF_STEP_GAME);
             questions[i] = number1 + " " + ops[index] + " " + number2;
 
             int result = 0;
