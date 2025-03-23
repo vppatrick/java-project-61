a84b7da8b5bc097eda801a8b7608ca5a57159143
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-23 19:28:53 +0300

    update Prime class

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Prime {
     public static final int COUNT_OF_STEP_GAME = 3;
     public static final int RANDOM_UPPER_LIMIT = 41;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     private static boolean isPrime(int n) {
         if (n < 2) {
             return false;
@@ -24,7 +24,7 @@ public class Prime {
         String[] answers = new String[COUNT_OF_STEP_GAME];
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
-            int number = random.nextInt(RANDOM_UPPER_LIMIT) + 1;
+            int number = RANDOM.nextInt(RANDOM_UPPER_LIMIT) + 1;
             questions[i] = Integer.toString(number);
             if (isPrime(number)) {
                 answers[i] = "yes";
