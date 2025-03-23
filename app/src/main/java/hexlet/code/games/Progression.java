e6540c7a3e7e4b8b22bbd3121183c82202980a3a
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-23 19:29:02 +0300

    update Progression class

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,7 +8,7 @@ public class Progression {
     public static final int RANDOM_UPPER_LIMIT = 91;
     public static final int MIN_RANDOM_NUMBER = 5;
     public static final int MAX_RANDOM_NUMBER = 10;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     public static void startGame() {
         String instruction = "What number is missing in the progression?";
         String[] questions = new String[COUNT_OF_STEP_GAME];
@@ -16,15 +16,15 @@ public class Progression {
 
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
-            int length = random.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER;
+            int length = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER;
 
-            int increment = random.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + 2;
+            int increment = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + 2;
 
             String[] progression = new String[length];
 
-            int indexOfHiddenItem = random.nextInt(length);
+            int indexOfHiddenItem = RANDOM.nextInt(length);
 
-            int initValue = random.nextInt(RANDOM_UPPER_LIMIT) + 2;
+            int initValue = RANDOM.nextInt(RANDOM_UPPER_LIMIT) + 2;
             progression[0] = Integer.toString(initValue);
 
             for (int j = 1; j < length; j++) {
