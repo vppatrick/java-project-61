96798c28ee3fd00907471ad0cbdd52e70c11d87c
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-23 19:28:47 +0300

    update GCD class

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class GCD {
     public static final int COUNT_OF_STEP_GAME = 3;
     public static final int RANDOM_UPPER_LIMIT = 100;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     private static int calcGCD(int a, int b) {
         return (a % b == 0) ? Math.abs(b) : calcGCD(b, a % b);
     }
@@ -16,8 +16,8 @@ public class GCD {
         String[] answers = new String[COUNT_OF_STEP_GAME];
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
-            int number1 = random.nextInt(RANDOM_UPPER_LIMIT);
-            int number2 = random.nextInt(RANDOM_UPPER_LIMIT);
+            int number1 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
+            int number2 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
             questions[i] = number1 + " " + number2;
             answers[i] = Integer.toString(calcGCD(number1, number2));
         }
