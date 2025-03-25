bf01dc06a5b08f858f790160513e9ac190309a54
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-25 20:28:46 +0300

    update GCD class

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -4,23 +4,24 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class GCD {
-    public static final int COUNT_OF_STEP_GAME = 3;
+    public static final int COUNT_OF_STEP_GAME = Engine.COUNT_OF_STEP_GAME;
     public static final int RANDOM_UPPER_LIMIT = 100;
     private static final Random RANDOM = new Random();
+
     private static int calcGCD(int a, int b) {
         return (a % b == 0) ? Math.abs(b) : calcGCD(b, a % b);
     }
+
     public static void startGame() {
         String instruction = "Find the greatest common divisor of given numbers.";
-        String[] questions = new String[COUNT_OF_STEP_GAME];
-        String[] answers = new String[COUNT_OF_STEP_GAME];
+        String[][] questionsAndAnswer = new String[COUNT_OF_STEP_GAME][2];
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
             int number1 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
             int number2 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
-            questions[i] = number1 + " " + number2;
-            answers[i] = Integer.toString(calcGCD(number1, number2));
+            questionsAndAnswer[i][0] = number1 + " " + number2;
+            questionsAndAnswer[i][1] = Integer.toString(calcGCD(number1, number2));
         }
-        Engine.start(instruction, questions, answers);
+        Engine.start(instruction, questionsAndAnswer);
     }
 }
