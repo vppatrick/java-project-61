4c99d8167987a1e9bec60b45255e555a3d63effd
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-25 20:28:40 +0300

    update Even class

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,23 +4,23 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Even {
-    public static final int COUNT_OF_STEP_GAME = 3;
+    public static final int COUNT_OF_STEP_GAME = Engine.COUNT_OF_STEP_GAME;
     public static final int RANDOM_UPPER_LIMIT = 100;
     private static final Random RANDOM = new Random();
+
     public static void startGame() {
         String instruction = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        String[] questions = new String[COUNT_OF_STEP_GAME];
-        String[] answers = new String[COUNT_OF_STEP_GAME];
+        String[][] questionsAndAnswer = new String[COUNT_OF_STEP_GAME][2];
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
             int number = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
-            questions[i] = Integer.toString(number);
+            questionsAndAnswer[i][0] = Integer.toString(number);
             if (number % 2 == 0) {
-                answers[i] = "yes";
+                questionsAndAnswer[i][1] = "yes";
             } else {
-                answers[i] = "no";
+                questionsAndAnswer[i][1] = "no";
             }
         }
-        Engine.start(instruction, questions, answers);
+        Engine.start(instruction, questionsAndAnswer);
     }
 }
