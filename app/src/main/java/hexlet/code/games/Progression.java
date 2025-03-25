31d918d3c796058672cbf6d48ea7e56edb48115d
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-25 20:28:59 +0300

    update Progression class

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,36 +4,38 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Progression {
-    public static final int COUNT_OF_STEP_GAME = 3;
+    public static final int COUNT_OF_STEP_GAME = Engine.COUNT_OF_STEP_GAME;
     public static final int RANDOM_UPPER_LIMIT = 91;
     public static final int MIN_RANDOM_NUMBER = 5;
     public static final int MAX_RANDOM_NUMBER = 10;
     private static final Random RANDOM = new Random();
+
+    private static String[] getProgression() {
+        int length = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER;
+        int increment = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + 2;
+        int initValue = RANDOM.nextInt(RANDOM_UPPER_LIMIT) + 2;
+
+        String[] result = new String[length];
+        result[0] = Integer.toString(initValue);
+
+        for (int i = 1; i < length; i++) {
+            result[i] = Integer.toString(Integer.parseInt(result[i - 1]) + increment);
+        }
+        return result;
+    }
+
     public static void startGame() {
         String instruction = "What number is missing in the progression?";
-        String[] questions = new String[COUNT_OF_STEP_GAME];
-        String[] answers = new String[COUNT_OF_STEP_GAME];
-
+        String[][] questionsAndAnswer = new String[COUNT_OF_STEP_GAME][2];
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
-            int length = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1) + MIN_RANDOM_NUMBER;
+            String[] progression = getProgression();
+            int indexOfHiddenItem = RANDOM.nextInt(progression.length);
 
-            int increment = RANDOM.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + 2;
-
-            String[] progression = new String[length];
-
-            int indexOfHiddenItem = RANDOM.nextInt(length);
-
-            int initValue = RANDOM.nextInt(RANDOM_UPPER_LIMIT) + 2;
-            progression[0] = Integer.toString(initValue);
-
-            for (int j = 1; j < length; j++) {
-                progression[j] = Integer.toString(Integer.parseInt(progression[j - 1]) + increment);
-            }
-            answers[i] = progression[indexOfHiddenItem];
+            questionsAndAnswer[i][1] = progression[indexOfHiddenItem];
             progression[indexOfHiddenItem] = "..";
-            questions[i] = String.join(" ", progression);
+            questionsAndAnswer[i][0] = String.join(" ", progression);
         }
-        Engine.start(instruction, questions, answers);
+        Engine.start(instruction, questionsAndAnswer);
     }
 }
