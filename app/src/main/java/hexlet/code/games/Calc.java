ee93c83f81fa119f9a6f01c15965a86983dde7df
Vitaliy Patrikeev <vppatric@gmail.com>
2025-03-25 20:28:34 +0300

    update Calc class

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,30 +4,33 @@ import hexlet.code.Engine;
 import java.util.Random;
 
 public class Calc {
-    public static final int COUNT_OF_STEP_GAME = 3;
+    public static final int COUNT_OF_STEP_GAME = Engine.COUNT_OF_STEP_GAME;
     public static final int RANDOM_UPPER_LIMIT = 10;
     private static final Random RANDOM = new Random();
+
+    private static int getResult(int a, int b, char op) {
+        int result = 0;
+        switch (op) {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            default: break;
+        }
+        return result;
+    }
+
     public static void startGame() {
         String instruction = "What is the result of the expression?";
-        String[] questions = new String[COUNT_OF_STEP_GAME];
-        String[] answers = new String[COUNT_OF_STEP_GAME];
+        String[][] questionsAndAnswer = new String[COUNT_OF_STEP_GAME][2];
         Character[] ops = {'+', '-', '*'};
 
         for (int i = 0; i < COUNT_OF_STEP_GAME; i++) {
             int number1 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
             int number2 = RANDOM.nextInt(RANDOM_UPPER_LIMIT);
-            int index = RANDOM.nextInt(COUNT_OF_STEP_GAME);
-            questions[i] = number1 + " " + ops[index] + " " + number2;
-
-            int result = 0;
-            switch (ops[index]) {
-                case '+': result = number1 + number2; break;
-                case '-': result = number1 - number2; break;
-                case '*': result = number1 * number2; break;
-                default: break;
-            }
-            answers[i] = Integer.toString(result);
+            int index = RANDOM.nextInt(ops.length);
+            questionsAndAnswer[i][0] = number1 + " " + ops[index] + " " + number2;
+            questionsAndAnswer[i][1] = Integer.toString(getResult(number1, number2, ops[index]));
         }
-        Engine.start(instruction, questions, answers);
+        Engine.start(instruction, questionsAndAnswer);
     }
 }
